7ef9cab618f63535afbad239b2ec429f2c051e93
Anton <ibavca@gmail.com>
2015-02-03 16:17:16 +0500

    Дабавил TestUnit, Infrastructure

---
+++ bavc.ru/WebParser/Infrastructure/Sites/WebPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebParser.Infrastructure.Sites
+{
+    class WebPage
+    {
+    }
+}
---
+++ bavc.ru/WebParser/Infrastructure/Sites/WebSite.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+using WebParser.Infrastructure.Sites;
+
+namespace WebParser.Infrastructure.Sites
+{
+    public abstract class WebSite
+    {
+        public abstract Encoding Encoding { get; }
+
+        public abstract Uri Uri { get; }
+
+        public abstract WebSiteMetadata Metadata { get; }
+
+        public virtual Uri MakeUri(string _siteRelativeUri)
+        {
+            Contract.Requires<ArgumentNullException>(_siteRelativeUri != null, "Не возможно создать пустой Uri");
+            Uri _uri = Uri;
+            NameValueCollection _siteQuery = HttpUtility.ParseQueryString(_uri.Query);
+            Uri uri;
+            bool IsAbsoluteUrl = Uri.TryCreate(_siteRelativeUri, UriKind.Absolute, out uri);
+            uri = IsAbsoluteUrl ? new Uri(_siteRelativeUri) : new Uri(_uri, _siteRelativeUri);
+            return uri;
+            //uri=uri
+        }
+    }
+}
