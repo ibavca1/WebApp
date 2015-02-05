89459f7fc4f77d7b5809d3d2551388960c036f8d
Anton <ibavca@gmail.com>
2015-02-05 16:53:57 +0500

    Создал модульное тестирование

---
+++ bavc.ru/UnitTestWebParser/UnitTestWebParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WebParser.BavcNetFramework;
+using WebParser.BavcNetFramework.DataTypes;
+
+namespace UnitTestWebParser
+{
+    [TestClass]
+    public class UnitTestWebParser
+    {
+        [TestMethod]
+        public void TestMethodCreate()
+        {
+            BasicParserWebRequest basicParser = new BasicParserWebRequest(WebRequestType.Http, new Uri("http://google1.com"));
+            HttpParserWebRequest httpParser = new HttpParserWebRequest();
+            WebPageContent content = httpParser.LoadPageContent(basicParser, Encoding.UTF8, TimeSpan.FromSeconds(1));
+            Assert.IsNotNull(content);
+        }
+    }
+}
